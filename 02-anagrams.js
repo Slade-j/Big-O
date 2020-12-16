3d009909b740cd1a73e32e97a75071f45c498002
Jason Slade <jason.slade906@gmail.com>
2020-12-16 15:45:17 -0700

    anograms

--- 02-anagrams.js
+++ 02-anagrams.js
@@ -10,19 +10,53 @@ function firstAnagram(str1, str2) {
   return Boolean(!strArr.length);
 }
 
-console.log(firstAnagram("elvis", "lives"));
+// console.log(firstAnagram("elvis", "lives"));
 
 
 
 function secondAnagram(str1, str2) {
-  // Code goes here ....
-}
+  let str1Arr = str1.split('');
+  let str2Arr = str2.split('');
 
+  str1Arr.sort();
+  str2Arr.sort();
+
+  return (str1Arr.join('') === str2Arr.join(''));
+}
+// console.log(secondAnagram("gizmo", "sally"));
 
 function thirdAnagram(str1, str2) {
-  // Code goes here ....
+  let str1Obj = {};
+  let str2Obj = {};
+
+  for (let i = 0; i < str1.length; i++) {
+    let char = str1[i];
+    if (str1Obj[char]) {
+      str1Obj[char]++
+    } else {
+      str1Obj[char] = 1
+    }
+  }
+
+  for (let i = 0; i < str2.length; i++) {
+    let char = str2[i];
+    if (str2Obj[char]) {
+      str2Obj[char]++
+    } else {
+      str2Obj[char] = 1
+    }
+
+  }
+
+  for (key in str1Obj) {
+    if (str1Obj[key] !== str2Obj[key]) {
+      return false;
+    }
+  }
+  return true;
 }
 
+console.log(thirdAnagram("gizmo", "sally"));
 
 function fourthAnagram(str1, str2) {
   // Code goes here ....
