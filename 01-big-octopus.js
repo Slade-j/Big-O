8b0f85870793401d1764e51e32e4b0ac7bcb8c45
Jason Slade <jason.slade906@gmail.com>
2020-12-16 14:47:04 -0700

    couple of functions

--- 01-big-octopus.js
+++ 01-big-octopus.js
@@ -1,17 +1,40 @@
 function quadraticBiggestFish(fishes) {
-  // Code goes here ...
-}
+  let longestFish = null;
+  for (let fish1 of fishes) {
+    for(let i = 0; i < fishes.length; i++) {
+      let fish2 = fishes[i];
+      if (fish1 <= fish2) {
+        longestFish = fish2
+      } else {
+        longestFish = fish1
+      }
+    }
 
+  }
+  return longestFish
+}
+const fishies = ['fish', 'fiiish', 'fiiiiish', 'fiiiish', 'fffish',
+'ffiiiiisshh', 'fsh', 'fiiiissshhhhhh']
+// console.log(quadraticBiggestFish(fishies));
 
 function nlognBiggestFish(fishes) {
-  // Code goes here ...
-}
+  fishes.sort((str1, str2) => str2.length - str1.length);
 
+  return fishes[0];
+}
+// console.log(nlognBiggestFish(fishies));
 
 function linearBiggestFish(fishes) {
-  // Code goes here ...
+  return fishes.reduce((accm, element) => {
+    if (accm.length > element.length){
+      return accm;
+    } else {
+      return element;
+    }
+  })
 }
 
+console.log(linearBiggestFish(fishies));
 
 tilesArray = ["up", "right-up", "right", "right-down", "down", "left-down", "left", "left-up"]
 function slowDance(direction, tilesArray) {
